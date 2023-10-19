49bfcd3f0b4f81b0ca44f7988fe20e99289ec07e
GeinerDuvan <131722054+GeinerDuvan@users.noreply.github.com>
2023-10-19 11:30:13 -0500

    Delete practica4/Assempler directory

--- practica4/Assempler/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        String nombreArchivoASM = args[0];
-        Ensamblador ensamblador = new Ensamblador(new AnalizadorSintactico(), new TablaSimbolos(), new Codigo());
-        ensamblador.ensamblar(nombreArchivoASM);
-    }
-}
